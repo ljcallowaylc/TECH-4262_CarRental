fde1384465c5a01b24092c9e48b6bae733f2fee8
Bryant Dixon <dixonbryant7@gmail.com>
2025-12-07 18:56:25 -0600

    database connection

--- Car Rental Management System/Data/CaeRentalContext.cs
+++ Car Rental Management System/Data/CaeRentalContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace Car_Rental_Management_System.Data
 {
     internal class CaeRentalContext
     {
+        // Update with your server & database
+        private static string connectionString =
+            "Server=SQLEXPRESS;Database=CarRentalDB;Trusted_Connection=True;";
+
+        // Returns an open SQL connection
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            return conn;
+        }
     }
 }
